fa3d48dc35c355339bc4e3312eabd34f0cba12e2
Drey <gatilhoroxo@gmail.com>
2025-09-18 16:05:08 -0300

    feat: inicio da cadeira

---
+++ listas/ex2/a_rails.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    stack<int> estacao;
+    int n=1;
+    cin >> n;
+    do {
+        int v=0;
+        while(true){
+            cin >> v;
+        }
+        cin >> n;
+    } while(n);
+
+    return 0;
+}
