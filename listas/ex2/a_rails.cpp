4bcbaf7cbd97b5d63f6aa47b27fe42f11ec37486
gatilhoroxo <gatilhoroxo@gmail.com>
2025-09-20 15:47:52 -0300

    tentando a 'a' ex2

--- listas/ex2/a_rails.cpp
+++ listas/ex2/a_rails.cpp
@@ -1,32 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     stack<int> estacao;
     int n=1;
-    while(true){
-        cin >> n;
-        if(n == 0) break;
-        else {
-            int v;
-            vector<int> numbers(n);
-            while(true){
-                cin >> v;
-                if(v == 0) break;
-                else {
-                    numbers[0] = v;
-                    for(int i=1; i<n; i++){
-                        cin >> numbers[i];
-                    }
-                    solve(numbers);
-                }
-            }
+    cin >> n;
+    do {
+        int v=0;
+        while(true){
+            cin >> v;
         }
-    }
+        cin >> n;
+    } while(n);
+
     return 0;
 }
