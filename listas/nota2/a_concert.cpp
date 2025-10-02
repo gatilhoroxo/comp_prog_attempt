b152c3babef22f192296df8224bdd2f8f88e9da2
gatilhoroxo <gatilhoroxo@gmail.com>
2025-10-01 22:11:44 -0300

    lista2: 2/3 da lista resolvida

--- listas/nota2/a_concert.cpp
+++ listas/nota2/a_concert.cpp
@@ -1,8 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define infinito (15 << 27)
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
+    int n_tic, m_cli; 
+    cin >> n_tic >> m_cli;
+    multiset<int> tickets;
+    for(int i=0; i<n_tic; i++){
+        int tic;
+        cin >> tic;
+        tickets.insert(tic);
+    }
+    
+    int clients;
+    for(int i=0; i<m_cli; i++){
+        cin >> clients;
+        auto it = tickets.upper_bound(clients);
+        //cout << *it << '\n';
+        if(it == tickets.begin()){
+            cout << -1 << '\n';
+        }
+        else {
+            cout << *(--it) << '\n';
+            tickets.erase(it);
+        }  
+    }
+
+    return 0;
 }
