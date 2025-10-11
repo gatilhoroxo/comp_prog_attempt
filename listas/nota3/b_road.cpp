d0a781241ac38e607adcd049a79dc9ab369043eb
Drey <gatilhoroxo@gmail.com>
2025-10-11 00:03:15 +0000

    feat(nota3): problema a feito

--- listas/nota3/b_road.cpp
+++ listas/nota3/b_road.cpp
@@ -1,5 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+using vi = vector<int>;
+//#define ll long long
+
+class DSU {
+private: vi p, rank;
+public:
+    DSU(int N) {
+        rank.assign(N, 0);
+        p.assign(N, 0);
+        for(int i=0; i<N; i++) {
+            p[i] = i;
+        }
+    }
+
+    int findSet(int i) {
+        return (p[i] == i) ? i : (p[i] = findSet(p[i])); 
+    }
+
+    bool isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+
+    void unionSet(int i, int j) {
+        if(!isSameSet(i, j)){
+            int x = findSet(i), y = findSet(j);
+            if(rank[x] > rank[y]) {
+                p[y] = x;
+            } else {
+                p[x] = y;
+                if(rank[x] == rank[y]) {
+                    rank[y]++;
+                }
+            }
+        }
+    }
+    
+};
+
 
 int main(){
     ios_base::sync_with_stdio(0);
