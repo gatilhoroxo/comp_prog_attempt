1489510ccce230cf569d60a7dd661be4b3d06a50
gatilhoroxo <gatilhoroxo@gmail.com>
2025-10-27 21:12:03 -0300

    trying(ex6/b)

--- listas/ex6/a_dominator.cpp
+++ listas/ex6/a_dominator.cpp
@@ -1,23 +1,77 @@
 //Dominator
+//nao sei pq esta dando wrong answer
 #include <bits/stdc++.h>
 using namespace std;
-using vb = vector<bool>;
 using ll = long long;
+using vb = vector<bool>;
 using vbb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+#define VISITED 1
+#define UNVISITED 0
+
+void dfs(vb& vt, vvi& adj, int u){
+    vt[u] = VISITED;
+    for(int j=0; j<adj[u].size(); j++){
+        if(adj[u][j] && vt[j] == UNVISITED ) { 
+            dfs(vt, adj, j); }
+    }
+}
+
+void dom(vvi& vt, vbb& ans){
+    dfs(ans[0], vt, 0);
+    for(int i=0; i<vt.size(); i++){
+        vb reach(vt.size(), UNVISITED);
+        
+        //tira i
+        auto buff = vt[i];
+        for(auto&i:vt[i]) i = 0;
+        dfs(reach, vt, 0);
+        vt[i] = buff;
+
+        for(int j=0; j<vt[i].size(); j++){
+            if(i==j) ans[i][j] = true;
+            else ans[i][j] = (!reach[j] && ans[0][j]);
+        }
+    }
+}
+
+void line(int n){
+    cout << "+";
+    for(int i=0; i<(2*n-1); i++){ cout << '-';}
+    cout << "+\n";
+}
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int t; cin >>t;
-    while(t--){
+    int tc; cin >>tc;
+    for(int t=0;t<tc;t++){
         int n; cin >> n;
-        vbb vt(n, vb(n));
+        vvi vt(n, vi(n));
         for(auto&i:vt){ 
-            for(int j=0; j<n; j++){ 
-                int ent;cin >> ent;
-                i[j]=ent; }}
+            for(auto&j:i){ 
+                cin >> j;
+            }
+        }
+
+        //process
+        vbb ans(n, vb(n,UNVISITED));
+        dom(vt, ans);
         
+        //saida
+        cout << "Case " << t +1 << ":\n"; 
+        for(int i=0; i<n; i++){ 
+            line(n); 
+            for(int j=0; j<n; j++){ 
+                cout << "|" << (ans[i][j] ? "Y" : "N");
+            } 
+            cout << "|\n";
+        } 
+        line(n);
+
     }
 
     return 0;
