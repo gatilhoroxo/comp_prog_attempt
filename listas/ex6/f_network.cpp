e42eabdeb06f716693a62e6038820fbad601289f
gatilhoroxo <gatilhoroxo@gmail.com>
2025-11-04 14:16:56 -0300

    feat(ex6): studying graphs problems

---
+++ listas/ex6/f_network.cpp
@@ -0,0 +1,17 @@
+//network
+//
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n=1; 
+    while(cin >> n){
+        
+    }
+
+    return 0;
+}
